abd62ad14ec880ebde144f87ad0820d98cdd4de9
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-10-05 20:24:54 +0200

    Reset default code

--- ticket-cart-checker/index.js
+++ ticket-cart-checker/index.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const app = express();
+const functions = require('@google-cloud/functions-framework');
 
-app.get('/', (req, res) => {
-    res.send("Root /")
+functions.http('main', (req, res) => {
+    // Use the url http://localhost:8080/?message=process1
+    if (req.query.message === "process1") {
+        res.send("You launched the process 1")
+        return;
+    }
+
+    // Use the url http://localhost:8080/?message=process2
+    if (req.query.message === "process2") {
+        res.send("You launched the process 2")
+        return;
+    }
+
+    // Use the url http://localhost:8080/?message=error
+    if (req.query.message === "error") {
+        res.send("error append")
+        return;
+    }
+    res.send(`${req.query.message || req.body.name || 'Welcome'}!`);
 });
-
-
-app.get('/hello', (req, res) => {
-    res.send("Root /hello")
-});
-
-exports.main = app;
