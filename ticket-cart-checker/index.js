9009e74b9d1d94d1e331a4a9abbb5fe1367e2709
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-10-11 14:38:29 +0200

    Edit package.json and faas

--- ticket-cart-checker/index.js
+++ ticket-cart-checker/index.js
@@ -1,10 +1,11 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.main = (req, res) => {
-    let message = req.query.message || req.body.message || 'ticket-cart-checker';
-    res.status(200).send(message);
-};
+const express = require('express');
+const app = express();
+app.use(express.json())
+
+
+app.get('/', async (req, res) => {
+   res.send("Welcome")
+})
+
+exports.main = app;
+
